b4bd2ae0437211c750d9ab55f2d5c2e56b4d10f8
tpct <eslamjackie@outlook.com>
2025-09-14 01:20:33 +0300

    adding fixies to car inner

--- Modules/Website/resources/views/layouts/master.blade.php
+++ Modules/Website/resources/views/layouts/master.blade.php
@@ -58,7 +58,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5 text-center " id="exampleModalLabel">{{__('lang.Sign in')}}</h1>
+                <h2 class="modal-title fs-5 text-center " id="exampleModalLabel">{{__('lang.Sign in')}}</h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -115,7 +115,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5 text-center " id="exampleModalLabel">{{__('lang.Sign up')}}</h1>
+                <h2 class="modal-title fs-5 text-center " id="exampleModalLabel">{{__('lang.Sign up')}}</h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
