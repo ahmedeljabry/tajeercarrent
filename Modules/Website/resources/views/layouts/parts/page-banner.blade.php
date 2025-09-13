5648ff05c0c4f0928d9bbef07bdca98faceb3310
tpct <eslamjackie@outlook.com>
2025-09-13 16:16:19 +0300

    adding fixes

--- Modules/Website/resources/views/layouts/parts/page-banner.blade.php
+++ Modules/Website/resources/views/layouts/parts/page-banner.blade.php
@@ -1,6 +1,6 @@
 <section class="new-pages-banner">
     <picture>
-        <img src="{{asset('/assets/images/car-subscription-new-image.png')}}" alt="{{$title}}">
+        <img src="{{ asset("/website/images/search_bg.webp") }}" alt="{{$title}}">
     </picture>
     <h2>{{$title}}</h2>
 </section>
